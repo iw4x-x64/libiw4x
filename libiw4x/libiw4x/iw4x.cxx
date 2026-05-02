6e44e0ed6ba33773ce812ee29230dc829ee875ee
William Roy <wroy@proton.me>
2026-05-02 17:11:42 -0400

    Avoid executing IW4x initialization under the loader lock

--- libiw4x/libiw4x/iw4x.cxx
+++ libiw4x/libiw4x/iw4x.cxx
@@ -1,5 +1,11 @@
 #include <libiw4x/iw4x.hxx>
 
+#include <array>
+#include <cstdint>
+#include <cstring>
+
+using namespace std;
+
 namespace iw4x
 {
   extern "C"
@@ -14,6 +20,54 @@ namespace iw4x
       //
       if (r != DLL_PROCESS_ATTACH)
         return TRUE;
+
+      // DllMain executes while the loader lock is held, so we defer IW4x
+      // initialization to the process's main thread to avoid ordering
+      // violation.
+      //
+      uintptr_t t (0x140358EBC);
+      uintptr_t s (reinterpret_cast<uintptr_t> (+[] ()
+      {
+        // __security_init_cookie
+        //
+        reinterpret_cast<void (*) ()> (0x1403598CC) ();
+
+        // __scrt_common_main_seh
+        //
+        return reinterpret_cast<int (*) ()> (0x140358D48) ();
+      }));
+
+      // Construct a 64-bit absolute jump. x64 has no single instruction for
+      // this, so we use `FF 25` (JMP [RIP+0]) followed by the 64-bit address.
+      //
+      array<unsigned char, 14> seq
+      ({
+        static_cast<unsigned char> (0xFF),
+        static_cast<unsigned char> (0x25),
+        static_cast<unsigned char> (0x00),
+        static_cast<unsigned char> (0x00),
+        static_cast<unsigned char> (0x00),
+        static_cast<unsigned char> (0x00),
+        static_cast<unsigned char> (s       & 0xFF),
+        static_cast<unsigned char> (s >> 8  & 0xFF),
+        static_cast<unsigned char> (s >> 16 & 0xFF),
+        static_cast<unsigned char> (s >> 24 & 0xFF),
+        static_cast<unsigned char> (s >> 32 & 0xFF),
+        static_cast<unsigned char> (s >> 40 & 0xFF),
+        static_cast<unsigned char> (s >> 48 & 0xFF),
+        static_cast<unsigned char> (s >> 56 & 0xFF)
+      });
+
+      DWORD o (0);
+      if (VirtualProtect (reinterpret_cast<void*> (t),
+                          seq.size (),
+                          PAGE_EXECUTE_READWRITE,
+                          &o) != 0)
+      {
+        memmove (reinterpret_cast<void*> (t), seq.data (), seq.size ());
+      }
+      else
+        return FALSE;
 
       // If we made it here, we are attached to the process.
       //
